58121882669ad892d0da741a14316dde3b8fff36
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-03-08 10:11:24 -0700

    add navigator files and set up in app.js

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Text, View } from 'react-native';
-import * as Front from 'expo-font';
+import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+import MealsNavigator from './navigation/MealsNavigator';
 
 const fetchFonts = () => {
-  Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold")
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
   });
 };
 
@@ -20,9 +21,5 @@ export default function App() {
     );
   };
 
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <MealsNavigator />;
 };
