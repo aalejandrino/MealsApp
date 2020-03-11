41f8ec9250636374a0e3f55b8939a72e3ae9a827
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-03-11 10:59:54 -0700

    set up category grid items

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = itemData => {
+    return (
+      <View style={styles.gridItem}>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+    
+}
+
 const CategoriesScreen = props => {
     return(
-        <FlatList numColumns={2} />
+        <FlatList 
+            keyExtractor={(item, index) => item.id} 
+            data={CATEGORIES} 
+            renderItem={renderGridItem} 
+            numColumns={2} />
     );
 };
 
@@ -12,6 +27,11 @@ const styles = StyleSheet.create({
         flex : 1,
         justifyContent : 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
