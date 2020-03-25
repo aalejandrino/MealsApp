ba711c69a6137c08fcf4dff39337fbd9e6e94e02
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-03-25 15:55:29 -0700

    add filters with switches

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,13 +1,56 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
 
+import Colors from "../constants/Colors";
+
+const FilterSwitch = props => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{
+          true: Colors.primaryColor
+        }}
+        thumbColor={Platform.OS === "android" ? Colors.primaryColor : "white"}
+        value={props.state}
+        onValueChange={props.onSwitch}
+      />
+    </View>
+  );
+};
+
 const FiltersScreen = props => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text> The Filters Screen! </Text>
+      <Text style={styles.title}> Available Filters / Restrictions </Text>
+      <FilterSwitch
+        label="Gluten-Free"
+        state={isGlutenFree}
+        onSwitch={newValue => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label="Lactose-Free"
+        state={isLactoseFree}
+        onSwitch={newValue => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        state={isVegan}
+        onSwitch={newValue => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onSwitch={newValue => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -33,8 +76,20 @@ FiltersScreen.navigationOptions = navData => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center"
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center"
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginVertical: 10
   }
 });
 
