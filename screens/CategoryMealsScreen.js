d3b79cec02d3eb7280540529a1662bf26825b5ad
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-03-29 10:37:55 -0700

    add fallback text for empty category meal lists

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 
 import { CATEGORIES } from "../data/dummy-data";
 
 import MealList from "../components/MealList";
+
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealsScreen = props => {
   const catId = props.navigation.getParam("categoryId");
@@ -13,6 +16,14 @@ const CategoryMealsScreen = props => {
   const displayedMeals = availableMeals.filter(meal => {
     return meal.categoryIds.indexOf(catId) >= 0;
   });
+
+  if (displayedMeals.length < 1)
+    return (
+      <View style={styles.empty}>
+        <DefaultText>Your meal list is empty.</DefaultText>
+        <DefaultText>Check your filters!</DefaultText>
+      </View>
+    );
 
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
@@ -26,5 +37,13 @@ CategoryMealsScreen.navigationOptions = navData => {
     headerTitle: selectedCategory.title
   };
 };
+
+const styles = StyleSheet.create({
+  empty: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default CategoryMealsScreen;
